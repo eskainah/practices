8a4e8fdd308a471c1699b1a40f4d94a3de09d609
eskainah <eskainah@gmail.com>
2024-07-11 02:08:53 +0300

    modules and library

---
+++ ALX Python Practices/main.py
@@ -0,0 +1,11 @@
+import calculator
+
+sum = calculator.add(5, 3)
+
+difference = calculator.substract(5, 3)
+
+product = calculator.multiply(5, 3)
+
+quotient = calculator.divide(5, 3)
+
+print(f"The sum is: {sum}. \nThe difference is: {difference}. \nThe product is: {product}. \nThe quotient is: {quotient}")
