c1daab69c2e7179ef812bc88f491de01398e7697
eskainah <eskainah@gmail.com>
2024-07-21 08:23:55 +0300

    Static Method for Utility Calculation

---
+++ ALX Python Practices/Class Method & Static Method/exercise2.py
@@ -0,0 +1,14 @@
+class MathUtils:
+    @staticmethod
+    def add(a, b):
+        return a + b
+
+    @staticmethod
+    def multiply(a, b):
+        return a * b
+
+# Usage
+print(MathUtils.add(5, 3))       # Output: 8
+print(MathUtils.multiply(5, 3))  # Output: 15
+
+
