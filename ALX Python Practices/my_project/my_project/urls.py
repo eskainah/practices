f2fb70f48c2e5557ae61fcd3e9e2b0ee0a272831
eskainah <eskainah@gmail.com>
2024-09-01 11:38:21 +0300

    set url path

--- ALX Python Practices/my_project/my_project/urls.py
+++ ALX Python Practices/my_project/my_project/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import path
+from my_app.views import BookListCreateAPIView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("api/books", BookListCreateAPIView.as_view(), name="book_list_create"),
 ]
